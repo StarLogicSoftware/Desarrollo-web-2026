06e27da5ab9c03b9f498997c4655a01b1cf34723
Nicolás Fumo <StarLogicSoftware@gmail.com>
2026-07-28 18:11:50 -0300

    Clase 2807

---
+++ Codigos de clase/Clase 19-20 - 2107/arrays.js
@@ -0,0 +1,12 @@
+
+//              0       1         2         3
+let frutas = ['pera','sandia','manzana','frutilla']
+
+
+console.log(frutas)
+console.log(frutas[2])
+
+frutas[2] = 'durazno'
+
+console.log(frutas)
+console.log(frutas[2])
